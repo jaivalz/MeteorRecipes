e5485f1f529df6c3d4c5ef2e0770a93e7dbeda35
Jaime <jaivalz@yahoo.com>
2016-02-27 17:07:57 +0100

    Added some menus, ingredients...

--- collections/Recipes.js
+++ collections/Recipes.js
@@ -1,4 +1,20 @@
-Recipes = new Meteor.Collection('recipes');
+Recipes = new Mongo.Collection('recipes');
+
+  Recipes.allow({
+    insert: function(userId, doc) {
+      return !!userId;
+    }
+  });
+
+Ingredient = new SimpleSchema({
+    name: {
+      type: String
+    },
+    amount: {
+      type: String
+    }
+
+});
 
 RecipeSchema = new SimpleSchema({
   name: {
@@ -9,6 +25,19 @@ RecipeSchema = new SimpleSchema({
   desc: {
     type: String,
     label: "Description"
+  },
+
+  ingredients: {
+    type: [Ingredient]
+  },
+
+  inMenu: {
+    type: Boolean,
+    defaultValue: false,
+    optional: true,
+    autoform: {
+      type: "hidden"
+    }
   },
 
   author: {
