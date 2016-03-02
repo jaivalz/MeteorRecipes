7487686db961a30e589797242818c1ea6ae8e05e
Jaime <jaivalz@yahoo.com>
2016-03-02 17:03:24 +0100

    Finished project

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -9,4 +9,10 @@ Template.Recipes.helpers({
   recipes: ()=> {
     return Recipes.find({});
   }
-})
+});
+
+Template.Recipes.events({
+	'click new-recipe': () => {
+		Session.set('newRecipe', true);
+	}
+});
